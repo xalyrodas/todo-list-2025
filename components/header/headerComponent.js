3fd933c92516c56d03facaa6eb3171cb8d2984a6
user <user@localhost.localdomain>
2025-07-27 14:50:33 -0600

    creacion de header

---
+++ components/header/headerComponent.js
@@ -0,0 +1,37 @@
+export function header(){
+    let header = document.createElement('header');
+
+    // Título principal
+    let h1 = document.createElement('h1');
+    h1.innerText = "TODO-LIST";
+    header.appendChild(h1);
+
+    // Contenedor de texto y número
+    let divTarea = document.createElement('div');
+    divTarea.className = "div-Tareas";
+
+    let texto = document.createElement('span');
+    texto.innerText = "pendientes";
+    texto.className = "texto";
+    divTarea.appendChild(texto);
+
+    let numero = document.createElement('span');
+    numero.innerText = "14";
+    numero.className = "numero";
+    divTarea.appendChild(numero);
+
+    header.appendChild(divTarea);
+
+    // Logo del usuario
+    let divLogoUsuario = document.createElement('div');
+    divLogoUsuario.className = "div-logo";
+
+    let img = document.createElement('img');
+    img.src = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2Fa3%2Fa0%2F44%2Fa3a044c397a292ac153c954fc5fcb739.jpg&f=1&nofb=1&ipt=06f33a6942f35bd1b9b79186ec437cec2e00bb28844efad80e38e3ca258d4445";
+    img.className = "logo-img";
+
+    divLogoUsuario.appendChild(img);
+    header.appendChild(divLogoUsuario);
+
+    return header;
+}
--- views/dashboardView.js
+++ views/dashboardView.js
@@ -1,10 +1,13 @@
+import { header } from "../components/header/headerComponent.js";
+
 export function dashboard() {
+
     let seccion = document.createElement('section');
 
-    let h1 = document.createElement('h1');
-    h1.innerHTML = "Hola Mundo"
-    seccion.appendChild(h1);
 
-    return seccion;
+    //header
+    seccion.appendChild(header())
+
+    return seccion
 }
 document.body.appendChild(dashboard());
